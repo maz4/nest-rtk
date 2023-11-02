7d67d5f0989415fc7a65d1fe2f8d023976c4ae9d
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2023-11-03 00:58:27 +0900

    add user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data) {
+    return await this.prismaService.user.create({ data });
+  }
+
+  async findUsers() {
+    return await this.prismaService.user.findMany();
+  }
+
+  async findUserById(id: number) {
+    return await this.prismaService.user.findUnique({
+      where: { id },
+    });
+  }
+}
