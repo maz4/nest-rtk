5af53c32548b543049ffcfe2c8c3d1ca4a79f660
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2023-11-03 00:59:01 +0900

    Add function to generate swagger file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { writeFileSync } from 'fs';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestRTK')
+    .setDescription('The example Nest RTK project')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+  writeFileSync('./swagger-spec.json', JSON.stringify(document, null, 2));
+
   await app.listen(3000);
 }
 bootstrap();
