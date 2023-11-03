0dd2663cd21c142bf3acd6030aec1b73682cd592
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2023-11-03 14:05:31 +0900

    update port for nest app

--- server/src/main.ts
+++ server/src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   writeFileSync('./swagger-spec.json', JSON.stringify(document, null, 2));
 
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
