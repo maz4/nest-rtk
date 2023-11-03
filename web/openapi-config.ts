7ad30952b7c846fcd649b59fa8e85d72db19d5cb
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2023-11-03 14:39:35 +0900

    add openapi config file with selected endpoints

---
+++ web/openapi-config.ts
@@ -0,0 +1,17 @@
+import type { ConfigFile } from "@rtk-query/codegen-openapi";
+
+const config: ConfigFile = {
+  schemaFile: "../server/swagger-spec.json",
+  apiFile: "./src/store/api/baseApi.ts",
+  apiImport: "baseSplitApi",
+  argSuffix: "Args",
+  responseSuffix: "Response",
+  outputFiles: {
+    "./src/store/api/gen/users.gen.ts": {
+      filterEndpoints: [/User/],
+    },
+  },
+  hooks: true,
+};
+
+export default config;
