d14b0880ed0ca43acc4f95ec210a00c870c6fd82
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2023-11-03 22:05:50 +0900

    update baseApi naming and update the fetchBaseQuery setup

--- web/src/store/api/baseApi.ts
+++ web/src/store/api/baseApi.ts
@@ -1,9 +1,15 @@
-// Or from '@reduxjs/toolkit/query' if not using the auto-generated hooks
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseApiUrl = "http://localhost:4000";
 // initialize an empty api service that we'll inject endpoints into later as needed
-export const baseSplitApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: baseApiUrl }),
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: baseApiUrl,
+    prepareHeaders: async (headers) => {
+      return headers;
+    },
+    credentials: "include",
+  }),
   endpoints: () => ({}),
 });
