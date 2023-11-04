d8c0126481553a4c0c6ad8943ea467d719c027e5
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2023-11-04 12:33:02 +0900

    add user repository to app module providers

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserService } from './user/user.service';
-import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma.service';
+import { UserModule } from './user/user.module';
+import { UserRepository } from './user/user.repository';
+import { UserService } from './user/user.service';
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [PrismaService, AppService, UserService],
+  providers: [AppService, PrismaService, UserRepository, UserService],
 })
 export class AppModule {}
