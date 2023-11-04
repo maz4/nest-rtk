6ecb256023e73b91b028a43d324b1d1d6a1d2f5e
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2023-11-04 12:32:37 +0900

    add user repository to user module providers

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
 import { PrismaService } from '../prisma.service';
+import { UserController } from './user.controller';
+import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [PrismaService, UserService],
+  providers: [PrismaService, UserRepository, UserService],
 })
 export class UserModule {}
